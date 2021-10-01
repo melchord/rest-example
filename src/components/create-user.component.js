06a26d8ad46c4884c448cb9a781a6e4d3cd775ad
Melchor Dominguez <mdmngz411@gmail.com>
2021-09-30 20:04:02 -0400

    - create user form

--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -2,8 +2,22 @@ import React, { Component } from 'react';
 
 export default class CreateUser extends Component {
   render() {
-    <div>
-      <p>Create User Component!!</p>
-    </div>;
+    return (
+      <div className='wrapper'>
+        <form>
+          <div className='form-group'>
+            <label>Enter Name</label>
+            <input type='text' className='form-control' />
+          </div>
+          <div className='form-group'>
+            <label>Enter Email</label>
+            <input type='text' className='form-control' />
+          </div>
+          <div className='form-group'>
+            <input type='submit' value='Create User' className='btn btn-success btn-block' />
+          </div>
+        </form>
+      </div>
+    );
   }
 }
